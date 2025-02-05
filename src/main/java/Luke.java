11d71ce07dadbbcf145271803c2d2bade3eaf0b6
Brian Goh <naeunonly1999@gmail.com>
2025-02-05 14:32:11 +0800

    Completed Level-2

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Luke {
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -17,13 +20,27 @@ public class Luke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 isRunning = false;
+            } else if (input.equals("list")) {
+                listTasks();
             } else {
-                System.out.println(input);
+                tasks[taskCount] = input;
+                System.out.println("added: " + input);
+                taskCount++;
             }
             printLine();
         }
-
         scanner.close();
+    }
+
+    private static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println("No tasks in your list!");
+            return;
+        }
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
     }
 
     private static void printLine() {
