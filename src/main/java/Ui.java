96c62cc16ccbef8f882b69fd9572c70712508f88
Brian Goh <naeunonly1999@gmail.com>
2025-02-12 14:46:51 +0800

    OOP

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("Hello! I'm Luke");
+        System.out.println("What can I do for you today?");
+        showLine();
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showLoadingError() {
+        System.out.println("Something went wrong loading the file!");
+    }
+
+    public void showSavingError() {
+        System.out.println("Something went wrong saving the file!");
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public void showBye() {
+        System.out.println("Bye. Hope to see you again!!");
+    }
 }
